3a9f13c06d1c120e43749fb0e251ff61a91265d7
glauberbrack <glauber@brack.com.br>
2020-09-21 10:09:48 -0300

    creating service-b

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { zip } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/ping-a")
+  pingServiceA() {
+    return this.appService.pingServiceA();
+  }
+
+  @Get("/ping-b")
+  pingServiceB() {
+    return this.appService.pingServiceB();
+  }
+
+  @Get("/ping-all")
+  pingAll() {
+    return zip(
+      this.appService.pingServiceA(),
+      this.appService.pingServiceB()
+    ).pipe(
+      map(([pongServiceA, pongServiceB]) => ({
+        pongServiceA,
+        pongServiceB
+      }))
+    );
   }
 }
--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: "SERVICE_A",
+        transport: Transport.TCP,
+        options: {
+          host: "127.0.0.1",
+          port: 8888
+        }
+      }
+    ])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
+
 export class AppModule {}
